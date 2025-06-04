926020c0a8c4085b0217dd9a2dd0e8062777e427
futo aoi <faoi0803@icloud.com>
2025-06-04 20:28:13 +0900

    代償攻撃のバフ調整

--- Assets/Futo/Compensation.cs
+++ Assets/Futo/Compensation.cs
@@ -11,20 +11,21 @@ public class CompensationEffect : UniqueEffect
     //カードの効果処理
     public override void Execute(Card card, Card flontCard, Battler player, Enemy enemy, Text message)
     {
-        int Hit = (int)(compensationValue * Random.Range(0.8f, 1.2f));
+        int attackValue = (int)card.Base.CardStatus.Attack_Status;  
+
+        int Hit = (int)(attackValue * compensationValue * Random.Range(0.8f, 1.2f));
         float defense = 1f - enemy.Base.EnemyDefense / 100f;
         int damage = (int)(Hit * defense);
         enemy.Base.EnemyLife -= damage;
-        message.text = $"{damage}ダメージ与えた";
         if (enemy.Base.EnemyLife < 0)
         {
             enemy.Base.EnemyLife = 0;
         }
         player.Life -= compensationDamage;
-        message.text = $"代償として{compensationDamage}を受けた";
         if (player.Life < 0)
         {
             player.Life = 0;
         }
+        message.text = $"{damage}ダメージ与えた\n代償として{compensationDamage}を受けた";
     }
 }
