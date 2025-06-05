1c59a81aefc7c45f2e80f6bbe6a5a56fdd546792
futo aoi <faoi0803@icloud.com>
2025-06-05 18:10:58 +0900

    雷撃の固定ダメージを与える処理を追加

---
+++ Assets/Futo/Thunder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "UniqueEffects/Thunder")]
+public class ThunderEffect : UniqueEffect
+{
+    [SerializeField, Range(5, 50)] int thunderDamage;
+    public override void Execute(Card card, Card flontCard, Battler player, Enemy enemy, Text message)
+    {
+        enemy.Base.EnemyLife -= thunderDamage;
+        if(enemy.Base.EnemyLife < 0)
+        {
+            enemy.Base.EnemyLife = 0;
+        }
+        message.text = $"{thunderDamage}ダメージ与えた";
+    }
+}
