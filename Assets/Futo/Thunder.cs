3dd1eb6a52f5d38a9bc3975de990da919a70118a
futo aoi <faoi0803@icloud.com>
2025-06-08 15:07:24 +0900

    雷撃の行動不能効果の追加

--- Assets/Futo/Thunder.cs
+++ Assets/Futo/Thunder.cs
@@ -7,13 +7,31 @@ using UnityEngine.UI;
 public class ThunderEffect : UniqueEffect
 {
     [SerializeField, Range(5, 50)] int thunderDamage;
+    public bool _isThunder = false;
+    [SerializeField, Range(1, 5)] int absoluteThunder = 3;
+    [SerializeField] int thunderCount = 0;
+    [SerializeField] int isThunderpercent = 5;
+
     public override void Execute(Card card, Card flontCard, Battler player, Enemy enemy, Text message)
     {
         enemy.Base.EnemyLife -= thunderDamage;
-        if(enemy.Base.EnemyLife < 0)
+        if(enemy.Base.EnemyLife < absoluteThunder)
         {
             enemy.Base.EnemyLife = 0;
         }
         message.text = $"{thunderDamage}ダメージ与えた";
+        thunderCount++;
+        if (thunderCount < 3)
+        {
+            int R = Random.Range(0, 99);
+            if ( R < isThunderpercent )
+            {
+                _isThunder = true ;
+            }
+        }
+        if( thunderCount >= absoluteThunder)
+        {
+            _isThunder = true;
+        }
     }
 }
